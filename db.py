cb31f3f1938c474f0deddff1c93a7615f55a1bb3
kalanaashen <northernwolf1472@gmail.com>
2026-02-10 22:40:24 +0530

    fix sql query

--- db.py
+++ db.py
@@ -6,15 +6,17 @@ def get_connection():
 
 def get_customers_for_reminder():
     sql_query="""SELECT 
-    u.email,
-    u.user_name,
+    p.email,
+    p.full_name,
     a.appointment_date,
     a.start_time,
-    a.treatment_type 
-FROM `user` u
-INNER JOIN appointment a ON u.user_id = a.patient_id;"""
+    a.treatment_type
+FROM appointment a
+INNER JOIN patient p 
+    ON a.patient_id = p.patient_id;
+"""
     conn=get_connection()
-    cursor=conn.cursor()
+    cursor=conn.cursor(dictionary=True)
     cursor.execute(sql_query)
     result=cursor.fetchall()
     cursor.close()
---
+++ email_service.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.message import EmailMessage
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+
+def send_email(to_email, subject, body):
+    msg = EmailMessage()
+    msg["From"] = EMAIL_USER
+    msg["To"] = to_email
+    msg["Subject"] = subject
+    msg.set_content(body)
+
+    with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as server:
+        server.starttls()
+        server.login(EMAIL_USER, EMAIL_PASSWORD)
+        server.send_message(msg)

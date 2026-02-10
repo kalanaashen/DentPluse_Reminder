692a0a6d7a10a57448b691f80c45547e4fd1d24a
kalanaashen <northernwolf1472@gmail.com>
2026-02-10 15:34:31 +0530

    fix scrape gmails from database

--- db.py
+++ db.py
@@ -3,3 +3,22 @@ from config import DB_CONFIG
 
 def get_connection():
     return mysql.connector.connect(**DB_CONFIG)
+
+def get_customers_for_reminder():
+    sql_query="""SELECT 
+    u.email,
+    u.user_name,
+    a.appointment_date,
+    a.start_time,
+    a.treatment_type 
+FROM `user` u
+INNER JOIN appointment a ON u.user_id = a.patient_id;"""
+    conn=get_connection()
+    cursor=conn.cursor()
+    cursor.execute(sql_query)
+    result=cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return result
+    
+                  
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from db import get_connection
+from db import get_connection, get_customers_for_reminder
 
 conn = get_connection()
 print("✅ Database connected")
 conn.close()
+
+
+result = get_customers_for_reminder()
+for row in result:
+    print(row)
